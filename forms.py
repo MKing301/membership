34d0e031b10124da6510fc02f8e6311b7934c821
MKING301 <33675411+MKing301@users.noreply.github.com>
2018-11-05 18:15:39 -0500

    Added forms.py with registration form Signed-off-by: MKING301

---
+++ forms.py
@@ -0,0 +1,18 @@
+"""[summary.
+
+[description]
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators
+
+
+# Register Class
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', [validators.Length(min=2, max=50)])
+    last_name = StringField('Last Name', [validators.Length(min=2, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Email(message="Invalid email!")])
+    password = PasswordField('Password', [
+        validators.equal_to('confirm', message='Passwords do not match.')])
+    confirm = PasswordField('Confirm Password')
