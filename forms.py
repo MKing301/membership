65aa42d533eb244b74bfe88e76aa3e9b376bee27
MKING301 <33675411+MKing301@users.noreply.github.com>
2018-11-12 16:34:43 -0500

    Added member form to forms.py Signed-off-by: MKING301

--- forms.py
+++ forms.py
@@ -4,7 +4,8 @@
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, DateField, IntegerField
+from wtforms import validators
 
 
 # Register Class
@@ -22,3 +23,21 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
+
+
+# Member Form Class
+class MemberForm(FlaskForm):
+    first_name = StringField('First Name', [validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.DataRequired()])
+    street_num = StringField('Street Number', [validators.DataRequired()])
+    street_name = StringField('Street Name', [validators.DataRequired()])
+    city = StringField('City', [validators.DataRequired()])
+    state = StringField('State', [validators.DataRequired()])
+    postal_code = StringField('Postal Code', [validators.DataRequired()])
+    contact_num = StringField('Contact Number', [validators.DataRequired()])
+    birthdate = DateField('Birthdate', format='%Y-%m-%d')
+    member_tier = IntegerField('Member Tier', [validators.DataRequired()])
+    assigned_elder_first_name = StringField(
+        'Assigned Elder First Name', [validators.DataRequired()])
+    assigned_elder_last_name = StringField(
+        'Assigned Elder Last Name', [validators.DataRequired()])
