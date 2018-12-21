55a0463620019a7c7c7faa0055237acb3086d36b
MKING301 <33675411+MKing301@users.noreply.github.com>
2018-12-21 13:23:16 -0500

    Updated code formatting Signed-off-by: MKING301

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,4 @@
 """Initial database setup.
-
 This module will establish the new table in the database for the membership
 app for members.
 """
@@ -9,15 +8,16 @@ import psycopg2
 import psycopg2.extras
 
 # Get a connection
-conn = psycopg2.connect(database=os.environ.get('DB_NAME'),
+conn = psycopg2.connect(host='localhost',
+                        database=os.environ.get('DB_NAME'),
                         user=os.environ.get('DB_USER'),
-                        password=os.environ.get('DB_PASSWORD'),
-                        host='localhost')
+                        password=os.environ.get('DB_PASSWORD'))
+
 # conn.cursor will return a cursor object, you can use this cursor to
 # perform queries
-dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+dic_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-dict_cur.execute(
+dic_cur.execute(
     '''CREATE TABLE members(
         member_id SERIAL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -32,8 +32,7 @@ dict_cur.execute(
         member_tier INTEGER,
         assigned_elder_first_name VARCHAR(50),
         assigned_elder_last_name VARCHAR(50)
-        )'''
-)
+        )''')
 
 # commit to db
 conn.commit()
