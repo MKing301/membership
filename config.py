d2b66587dabda97f8b39201e85695dfca7c1f2bd
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-04-14 18:39:28 -0400

    Removed debug from config file

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 """This is the configuration file for the application."""
 import os
 
-DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY_LOCAL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.environ.get('LOCAL_DB_URI_MEMBERSHIP')
