d39bb099e8227fa182334cc41419e1f6f4c04cdc
MKING301 <33675411+MKing301@users.noreply.github.com>
2020-05-13 22:19:20 -0400

    Refactor database setup

--- database_setup.py
+++ database_setup.py
@@ -1,23 +1,23 @@
-"""Initial database setup.
-This module will establish the new table in the database for the membership
-app for members.
+"""Database setup.
+This module will establish the new tables in the database for the
+membership app.
 """
 
 import os
 import psycopg2
 import psycopg2.extras
+from helpers import connect
+from datetime import datetime
+from passlib.hash import sha256_crypt
 
-# Get a connection
-conn = psycopg2.connect(host='localhost',
-                        database=os.environ.get('DB_NAME'),
-                        user=os.environ.get('DB_USER'),
-                        password=os.environ.get('DB_PASSWORD'))
 
-# conn.cursor will return a cursor object, you can use this cursor to
-# perform queries
-dic_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+first_name = os.environ.get('FIRST_NAME')
+last_name = os.environ.get('LAST_NAME')
+username = os.environ.get('USERNAME')
+email = os.environ.get('EMAIL')
+hashed_password = sha256_crypt.hash(str(os.environ.get('PASSWORD')))
 
-dic_cur.execute(
+create_queries = [
     '''CREATE TABLE members(
         member_id SERIAL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -25,19 +25,96 @@ dic_cur.execute(
         street_num VARCHAR(10),
         street_name VARCHAR(50),
         city VARCHAR(50),
-        state VARCHAR(2),
+        _state VARCHAR(2),
         postal_code VARCHAR(10),
         contact_num VARCHAR(10),
-        birthdate DATE,
-        member_tier INTEGER,
-        assigned_elder_first_name VARCHAR(50),
-        assigned_elder_last_name VARCHAR(50)
-        )''')
+        email VARCHAR (50),
+        birthdate DATE
+        )''',
+    '''CREATE TABLE admins(
+        admin_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        username VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        password TEXT,
+        role VARCHAR(6) NOT NULL,
+        registered_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        )'''
+]
 
-# commit to db
+for query in create_queries:
+    # Get a connection
+    conn, cur = connect()
+
+    # Execute query
+    cur.execute(query)
+
+    # Commit to db
+    conn.commit()
+
+    # Close cursor
+    cur.close()
+
+    # Close db connection
+    conn.close()
+
+# Insert initial admin
+# Get a connection
+conn, cur = connect()
+
+# Execute query
+cur.execute('''INSERT INTO admins(
+    first_name,
+    last_name,
+    username,
+    email,
+    password,
+    role,
+    registered_date)
+    VALUES(%s, %s, %s, %s, %s ,%s, %s)
+''', (first_name,
+      last_name,
+      username,
+      email,
+      hashed_password,
+      'admin',
+      datetime.now()))
+
+
+# Insert initial admin
+
+# Execute query
+cur.execute('''INSERT INTO members(
+        first_name,
+        last_name,
+        street_num,
+        street_name,
+        city,
+        _state,
+        postal_code,
+        contact_num,
+        email,
+        birthdate)
+      VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ''', ('JoAnn',
+          'Walker',
+          '777',
+          'Main St',
+          'Raleigh',
+          'NC',
+          '27610',
+          '9197779311',
+          'jwalk@gmail.com',
+          '1981-08-28'))
+
+# Commit to db
 conn.commit()
 
-# close db connection
+# Close cursor
+cur.close()
+
+# Close db connection
 conn.close()
 
-print("Members table created successfully!")
+print("Database setup complete!")
